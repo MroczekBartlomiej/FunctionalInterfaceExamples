ea11ea450e6de67d925cacaa99fee8ff57f1efa6
MroczekBartlomiej <mroczek.bartlomiej@gmail.com>
2017-07-09 12:21:21 +0200

    Function expample

--- src/main/java/FunctionalInterface.java
+++ src/main/java/FunctionalInterface.java
@@ -1,7 +1,34 @@
+import java.util.function.Function;
 
 public class FunctionalInterface {
 
     public static void main(String[] args) {
+
+        Function<Integer, Integer> factorial = (f) -> {
+            int result = 1;
+            for (int i = 1; i <= f; i++) {
+                result = result * i;
+            }
+            return result;
+        };
+
+        Function<Integer, String> numberChecker = (f) -> {
+            String result = "Number is zero.";
+            if (f > 0) {
+                result = "Number is bigger than zero.";
+            }else if(f <0){
+                result = "Number is smaller than zero.";
+            }
+            return result;
+        };
+
+        System.out.println("Factorial of number " + 3 + " is " + factorial.apply(3));
+        System.out.println("Factorial of number " + 4 + " is " + factorial.apply(4));
+        System.out.println("Factorial of number " + 5 + " is " + factorial.apply(5));
+
+        System.out.println(numberChecker.apply(2));
+        System.out.println(numberChecker.apply(-1));
+        System.out.println(numberChecker.apply(0));
 
     }
 }
